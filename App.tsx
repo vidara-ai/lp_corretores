5963f9c047ff02500fdb8abe5a738211f1706d7e
vidara-ai <fluxxads@gmail.com>
2026-02-09 17:46:33 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import LandingHero from './components/LandingHero';
-import EditorialBlock from './components/EditorialBlock';
-import MarketAnalysisBlock from './components/MarketAnalysisBlock';
-import SolutionBlock from './components/SolutionBlock';
-import ProductPreviewBlock from './components/ProductPreviewBlock';
-import DifferentialsBlock from './components/DifferentialsBlock';
-import AuthorityBlock from './components/AuthorityBlock';
-import ClosingBlock from './components/ClosingBlock';
+import LandingHero from './components/LandingHero.tsx';
+import EditorialBlock from './components/EditorialBlock.tsx';
+import MarketAnalysisBlock from './components/MarketAnalysisBlock.tsx';
+import SolutionBlock from './components/SolutionBlock.tsx';
+import ProductPreviewBlock from './components/ProductPreviewBlock.tsx';
+import DifferentialsBlock from './components/DifferentialsBlock.tsx';
+import AuthorityBlock from './components/AuthorityBlock.tsx';
+import ClosingBlock from './components/ClosingBlock.tsx';
 
 const App: React.FC = () => {
   return (
